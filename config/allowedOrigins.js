870cdd5355f1449147d48e97781116aa4deab95b
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2024-10-26 08:59:19 +0200

    added the frontend to the allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const {all} = require("express/lib/application");
 const allowedOrigins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:5173'
 ]
 
 module.exports = allowedOrigins
