0d9871f5bf60120363ab390ccb99cfefc8925052
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-01-04 11:24:49 +0100

    cleaned up the model and added a name field to the notifications

--- models/notificationModel.js
+++ models/notificationModel.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    furnitureId: { type: mongoose.Schema.Types.ObjectId, ref: 'Furniture', required: true },
-    threshold: { type: Number, required: true },
-    comparison: { type: String, enum: ['LESS_THAN', 'GREATER_THAN'], required: true },
-    isTriggered: { type: Boolean, default: false },
+    name: {
+        type: String
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    furnitureId: { type: mongoose.Schema.Types.ObjectId,
+        ref: 'Furniture',
+        required: true
+    },
+    threshold: {
+        type: Number,
+        required: true
+    },
+    comparison: {
+        type: String,
+        enum: ['LESS_THAN', 'GREATER_THAN'],
+        required: true
+    },
+    isTriggered: {
+        type: Boolean,
+        default: false
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Notification', notificationSchema);
--- models/stockMovement.js
+++ models/stockMovement.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose');
 
 const stockMovementSchema = new mongoose.Schema({
-    furnitureId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    quantityChange: { type: Number, required: true },
-    movementType: { type: String, enum: ['IN', 'OUT'], required: true },
-    quantity: { type: Number, required: true },
-    modifiedBy: { type: String, required: true },
-    name: { type: String, required: true },
+    furnitureId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    quantityChange: {
+        type: Number,
+        required: true
+    },
+    movementType: {
+        type: String,
+        enum: ['IN', 'OUT'],
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    modifiedBy: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
 }, {
     timestamps: true,
 });
