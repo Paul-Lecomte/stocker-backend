5695d73da535a57546fafc9c28c0a8be93ca9039
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-06 08:34:55 +0100

    protected the aisle route

--- routes/aisleRoutes.js
+++ routes/aisleRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const { createAisle, deleteAisle, getAllAisles } = require('../controllers/aisleController');
+const {protect} = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // Route to create a new aisle
-router.post('/', createAisle);
+router.post('/',protect, createAisle);
 
 // Route to delete an aisle by ID
-router.delete('/:id', deleteAisle);
+router.delete('/:id',protect, deleteAisle);
 
 // Route to fetch all aisles
-router.get('/all', getAllAisles);
+router.get('/all',protect, getAllAisles);
 
 module.exports = router;
